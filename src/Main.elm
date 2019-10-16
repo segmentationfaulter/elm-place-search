377b6dd76cc1cd461052c88df9deb5383c9c83a4
segmentationfaulter <muhammadsackib@gmail.com>
2019-10-16 22:44:19 +0500

    format docs

--- src/Main.elm
+++ src/Main.elm
@@ -1,95 +1,123 @@
-import Browser
-import Html exposing(Html, Attribute, div, input)
-import Html.Attributes exposing(placeholder, type_, value)
-import Html.Events exposing(onInput)
-import String
-import Http
-import Json.Decode exposing(Decoder, list, field, decodeString, map2, string)
+module Main exposing (Model, Msg(..), PlacePrediction, getAutoCompleteSuggestions, getPlacesApiEndpoint, init, initialModel, main, placePredictionDecoder, placesPredictionsDecoder, subscriptions, update, view)
 
-main = Browser.element {
-        init = init,
-        view = view,
-        update = update,
-        subscriptions = subscriptions
-    }
+import Browser
+import Html exposing (Attribute, Html, div, input)
+import Html.Attributes exposing (placeholder, type_, value)
+import Html.Events exposing (onInput)
+import Http
+import Json.Decode exposing (Decoder, decodeString, field, list, map2, string)
+import String
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- Init
-init: () -> (Model, Cmd msg)
+
+
+init : () -> ( Model, Cmd msg )
 init () =
-    (initialModel, Cmd.none)
+    ( initialModel, Cmd.none )
+
 
 
 -- Modal
 
+
 type alias Model =
-    {
-        query: String
+    { query : String
     }
 
-initialModel: Model
+
+initialModel : Model
 initialModel =
-    {
-        query = ""
+    { query = ""
     }
+
+
 
 -- Update
+
 
 type Msg
     = QueryChanged String
     | PlacesPredictionsReceived (Result Http.Error (List PlacePrediction))
 
-update: Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg _ =
     case msg of
-        QueryChanged query -> ({ query = query }, getAutoCompleteSuggestions (getPlacesApiEndpoint query))
-        PlacesPredictionsReceived predictionsResult -> case predictionsResult of
-            Result.Ok result -> (initialModel, Cmd.none)
-            Result.Err _ -> (initialModel, Cmd.none)
+        QueryChanged query ->
+            ( { query = query }, getAutoCompleteSuggestions (getPlacesApiEndpoint query) )
+
+        PlacesPredictionsReceived predictionsResult ->
+            case predictionsResult of
+                Result.Ok result ->
+                    ( initialModel, Cmd.none )
+
+                Result.Err _ ->
+                    ( initialModel, Cmd.none )
+
+
 
 -- View
 
-view: Model -> Html Msg
+
+view : Model -> Html Msg
 view model =
-    div [] [
-        input [placeholder "Enter place name", type_ "text", value (String.toUpper model.query), onInput QueryChanged] []
-    ]
+    div []
+        [ input [ placeholder "Enter place name", type_ "text", value (String.toUpper model.query), onInput QueryChanged ] []
+        ]
+
+
 
 -- Subscription
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
+
 -- Helpers
 
-getPlacesApiEndpoint: String -> String
+
+getPlacesApiEndpoint : String -> String
 getPlacesApiEndpoint query =
     let
-        key = "AIzaSyClde8PpxKB0E6r5xoFf2LfDIpLNv047gw"
+        key =
+            "AIzaSyClde8PpxKB0E6r5xoFf2LfDIpLNv047gw"
     in
-        "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=" ++ key ++ "&input=" ++ query
+    "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=" ++ key ++ "&input=" ++ query
 
-getAutoCompleteSuggestions: String -> Cmd Msg
+
+getAutoCompleteSuggestions : String -> Cmd Msg
 getAutoCompleteSuggestions url =
     Http.get
-        {
-            url = url,
-            expect = Http.expectJson PlacesPredictionsReceived placesPredictionsDecoder
+        { url = url
+        , expect = Http.expectJson PlacesPredictionsReceived placesPredictionsDecoder
         }
 
 
 type alias PlacePrediction =
-    {
-        description: String,
-        place_id: String
+    { description : String
+    , place_id : String
     }
 
 
-placesPredictionsDecoder: Decoder (List PlacePrediction)
+placesPredictionsDecoder : Decoder (List PlacePrediction)
 placesPredictionsDecoder =
     field "predictions" (list placePredictionDecoder)
 
-placePredictionDecoder: Decoder PlacePrediction
+
+placePredictionDecoder : Decoder PlacePrediction
 placePredictionDecoder =
     map2 PlacePrediction (field "place_id" string) (field "description" string)
