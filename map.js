0ad728e084c7e8a18483df5c31573e31f592584e
segmentationfaulter <muhammadsackib@gmail.com>
2019-10-19 21:16:19 +0500

    use const

--- map.js
+++ map.js
@@ -1,21 +1,21 @@
 export function initMap(app) {
   return function() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: -33.8688, lng: 151.2195 },
       zoom: 10
     });
-    var input = document.getElementById("input-autocomplete");
-    var marker = new google.maps.Marker({ map: map });
+    const input = document.getElementById("input-autocomplete");
+    const marker = new google.maps.Marker({ map: map });
 
     app.ports.centerMap.subscribe(location => {
       map.setCenter(location);
       marker.setPosition(location);
     });
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
+    const autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.setFields(["geometry", "name"]);
     autocomplete.addListener("place_changed", function() {
-      var place = autocomplete.getPlace();
+      const place = autocomplete.getPlace();
       app.ports.onPlaceChange.send({
         name: place.name,
         location: {
